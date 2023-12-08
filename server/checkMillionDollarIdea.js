9a95e6707297da283f8a9c73a01dc37d6fc419da
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2023-12-08 16:40:38 +0000

    APIs complete, testing required

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,9 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+    const totalValue = numWeeks * weeklyRevenue;
+    const millionDollarIdea = (totalValue < 1000000) ? false : true;
+    
+    return millionDollarIdea;
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/minions.js
+++ server/minions.js
@@ -44,10 +44,10 @@ minionsRouter.post('/', (req, res, next) => {
 
 // PUT to edit minion by id
 minionsRouter.put('/:minionId', (req, res, next) => {
-    // const { error } = validateMinion(req.body);
-    // if (error) {
-    //     return res.status(400).send(error.details[0].message)
-    // }
+    const { error } = validateMinion(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
     const updatedMinion = updateInstanceInDatabase('minions', req.body);
     res.status(200).send(updatedMinion);
 });
@@ -57,6 +57,66 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
     const deletedMinion = deleteFromDatabasebyId('minions', req.minion.id);
     res.status(204).send(deletedMinion);
 });
+
+
+// --- Work Routes ---
+
+// GET all work by minion ID
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const allWork = getAllFromDatabase('work');
+    const selectedMinionsWork = allWork.filter(work => {
+        work.minionId === req.params.minionId
+    });
+    res.send(selectedMinionsWork);
+})
+
+// POST new work by minion ID
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    const { error } = validateWork(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    const work = req.body;
+    work.minionId = req.params.minionId;
+    const newWork = addToDatabase('work', work);
+    res.send(newWork);
+})
+
+// set work params
+minionsRouter.use('workId', (req, res, next, id) => {
+    const workId = getFromDatabaseById('work', id);
+    if (workId !== -1) {
+        req.work = workId;
+        next();
+    } else {
+        res.status(404).send('Work with given ID not found')
+    }
+})
+
+// PUT update minion work by work id
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    const { error } = validateWork(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    if (req.params.minionId !== req.body.minionId) {
+        return res.status(400).send('minion with given ID not found');
+    }
+
+    const updatedWork = updateInstanceInDatabase('work', req.body);
+    res.send(updatedWork);
+});
+
+// DELETE work by ID
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    if (req.params.minionId !== req.body.minionId) {
+        return res.status(400).send('minion with given ID not found');
+    }
+
+    const deletedWork = deleteFromDatabasebyId('work', req.params.workId);
+    res.send(deletedWork);
+})
 
 // Validation
 const validateMinion = (minion) => {
@@ -68,4 +128,15 @@ const validateMinion = (minion) => {
     });
 
     return schema.validate(minion);
+}
+
+const validateWork = (work) => {
+    const schema = Joi.object({
+        title: Joi.string().min(3).trim().required(),
+        description: Joi.string().min(3).trim().required(),
+        hours: Joi.number().required(),
+        minionId: Joi.string().required(),
+    });
+
+    return schema.validate(work);
 }
